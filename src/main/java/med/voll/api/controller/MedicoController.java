4c62befdfd4971c96ce947e890d4fad5cbc635ad
viniciuspadovam <padovam3373@gmail.com>
2023-08-08 23:39:35 -0300

    detalhamento de medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,11 +1,13 @@
 package med.voll.api.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +17,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosDetalhamentoMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.DadosListagemTodosOsMedicos;
 import med.voll.api.medico.Medico;
@@ -32,26 +36,36 @@ public class MedicoController {
 	private MedicoRepository repository;
 	
 	@GetMapping("/all")
-	public List<DadosListagemTodosOsMedicos> listAll() {
-		return repository.findAll().stream().map(DadosListagemTodosOsMedicos::new).toList();
+	public ResponseEntity<List<DadosListagemTodosOsMedicos>> listAll() {
+		List<DadosListagemTodosOsMedicos> list = repository.findAll().stream().map(DadosListagemTodosOsMedicos::new).toList();
+		return ResponseEntity.ok(list);
 	}
 	
 	@GetMapping
-	public Page<DadosListagemMedico> list(@PageableDefault(size = 10, sort = {"id"}) Pageable pagination) {
-		return repository.findAllByAtivoTrue(pagination).map(DadosListagemMedico::new);
+	public ResponseEntity<Page<DadosListagemMedico>> list(@PageableDefault(size = 10, sort = {"id"}) Pageable pagination) {
+		Page<DadosListagemMedico> list = repository.findAllByAtivoTrue(pagination).map(DadosListagemMedico::new);
+		return ResponseEntity.ok(list);
 	}
 	
 	@PostMapping
 	@Transactional
-	public void rigester(@RequestBody @Valid DadosCadastroMedico dados) {
-		repository.save(new Medico(dados));
+	public ResponseEntity<DadosDetalhamentoMedico> register(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
+		Medico medico = new Medico(dados);
+		
+		repository.save(medico);
+		
+		URI uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
 	}
 	
 	@PutMapping
 	@Transactional
-	public void update(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+	public ResponseEntity<DadosDetalhamentoMedico> update(@RequestBody @Valid DadosAtualizacaoMedico dados) {
 		var medico = repository.getReferenceById(dados.id());
 		medico.atualizarInformacoes(dados);
+		
+		return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
 	}
 	
 	/**
@@ -60,11 +74,21 @@ public class MedicoController {
 	 * como se tivesse sido excluido de fato. 
 	 *  
 	 * @param id
+	 * @return ResponseEntity - retorna "204 No Content"
 	 */
 	@DeleteMapping("/{id}")
 	@Transactional
-	public void delete(@PathVariable Long id) {
+	public ResponseEntity<?> delete(@PathVariable Long id) {
 		Medico medico = repository.getReferenceById(id);
 		medico.logicDelete();
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<DadosDetalhamentoMedico> detalhar(@PathVariable Long id) {
+		Medico medico = repository.getReferenceById(id);
+		
+		return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
 	}
 }
--- src/main/java/med/voll/api/medico/DadosListagemTodosOsMedicos.java
+++ src/main/java/med/voll/api/medico/DadosListagemTodosOsMedicos.java
@@ -1,7 +1,9 @@
 package med.voll.api.medico;
 
-public record DadosListagemTodosOsMedicos(Long id, String nome, String email, String crm, Especialidade especialidade, Boolean ativo) {
+import med.voll.api.endereco.Endereco;
+
+public record DadosListagemTodosOsMedicos(Long id, String nome, String email, String crm, Especialidade especialidade, Endereco endereco, Boolean ativo) {
 	public DadosListagemTodosOsMedicos(Medico medico) {
-		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade(), medico.getAtivo());
+		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco(), medico.getAtivo());
 	}
 }
