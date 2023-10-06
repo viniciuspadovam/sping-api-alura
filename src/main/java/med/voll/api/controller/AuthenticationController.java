d90de3dee6eb5ef557a45dccf158ecbc634f44da
viniciuspadovam <padovam3373@gmail.com>
2023-10-06 11:02:53 -0300

    refactor: retirado log auth controller

--- src/main/java/med/voll/api/controller/AuthenticationController.java
+++ src/main/java/med/voll/api/controller/AuthenticationController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +28,6 @@ public class AuthenticationController {
 	
 	@PostMapping
 	public ResponseEntity<JWTTokenData> login(@RequestBody @Valid DadosAutenticacao data) {
-		System.out.println(new BCryptPasswordEncoder().encode(data.password()));
 		Authentication authToken = new UsernamePasswordAuthenticationToken(data.login(), data.password());
 		Authentication auth = manager.authenticate(authToken);
 		
